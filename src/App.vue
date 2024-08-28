fada33ba8ee5bcb9137f91b95a952d335a862f3b
Jatikhmer <jatikhmer@gmail.com>
2024-08-28 08:32:23 -0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <nav class="bg-gray-200 p-2">
+    <div
+      class="container mx-auto flex flex-col md:flex-row justify-between gap-2"
+    >
+      <div>
+        <h1 class="text-lg md:text-xl lg:text-2xl xl:text-3xl font-bold">Khmer Chhankitek Calendar</h1>
+      </div>
+      <div>
+        <ul class="flex gap-2">
+          <li>
+            <RouterLink to="/">Home</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/author">Thanks to</RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="container mx-auto">
+    <RouterView />
+  </div>
+</template>
---
+++ src/components/TheHolidaysList.vue
@@ -0,0 +1,30 @@
+<script setup>
+import moment from "moment/min/moment-with-locales";
+
+defineProps({
+  events: Object,
+});
+</script>
+
+<template>
+  <ol class="relative border-l border-gray-200 font-hanuman">
+    <template v-for="(holiday, index) in events" :key="index">
+      <template v-if="holiday.customData.title.en !== 'today'">
+        <li class="ml-4 mt-4">
+          <div
+            class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white"
+          ></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-400"
+            >{{ moment(holiday.dates).locale("km").format("dddd ll") }}
+          </time>
+          <h3 class="text-lg font-semibold text-gray-900">
+            {{ holiday.customData.title.kh }}
+          </h3>
+          <p class="text-base font-normal text-gray-500 dark:text-gray-400">
+            {{ holiday.customData.title.en }}
+          </p>
+        </li>
+      </template>
+    </template>
+  </ol>
+</template>
